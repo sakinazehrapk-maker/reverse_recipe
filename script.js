daf00464654bec3d87eb63d8bb3c986e6104477d
sakina <sakinazehrapk@gmail.com>
2026-06-03 01:22:53 +0500

    Added animated achievement popup notifications

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 console.log("script loaded");
 
 let brewCount = 0;
-
 const achievements = new Set();
+function showAchievementPopup(name){
+    const popup =
+    document.getElementById(
+        "achievementPopup"
+    );
+    const popupText=
+    document.getElementById(
+        "achievementPopupText"
+    );
+    popupText.innerText=
+    `Achievement unlocked!<br>${name}`;
+    popup.classList.add("show");
+    setTimeout(()=>{
+        popup.classList.remove("show";)
+    },3000)
+}
+
 function unlockAchievement(name){
     if(achievements.has(name)){
         return;
@@ -10,12 +26,34 @@ function unlockAchievement(name){
     achievements.add(name);
     const list =
         document.getElementById("achievementList");
-    if(list.children[0]?.textContent === "None Yet..."){
-        list.innerHTML = "";
+
+    if (
+        list&&
+        list.textContent.trim()=== "none yet..."
+    ){
+        list.innerHTML="";
     }
+    
+    if(list){
     const item = document.createElement("li");
     item.textContent = name;
     list.appendChild(item);
+    }
+    checkArchmage();
+}
+function checkArchmage(){
+    if(
+        achievements.has("First Brew")&&
+        achievements.has("Curious Apprentice")&&
+        achievements.has("Position Enthusiast")&&
+        achievements.has("Arcane Researcher")&&
+        achievements.has("Reality Breaker")&&
+        achievements.has("Eldritch Chef")
+    ){
+        if(!achievements.has("Archmage of Chaos")){
+            unlockAchievement("Archmage of Chaos");
+        }
+    }
 }
 
 const results = [
@@ -38,53 +76,45 @@ const forbiddenResults = [
     "THE CAULDRON IS AFRAID OF YOU",
     "YOU HAVE COOKED SOMETHING THAT SHOULD NOT EXIST"
 ];
+const lorePages =[
+    "PAGE:17 never mix moon ilk with dragon ketchup,the kingdom is still recovering",
+    "PAGE 42:wizard Gary attempted to season soup with pure magic.we do not speak of Gary",
+    "PAGE 88: the great banana incident lasted three winters",
+    "PAGE 103:if your ccauldron begins singing, evacuate immediately",
+    "PAGE 144: the council banned glowing mayonnaise after the incident",
+    "PAGE 207:a potion containing seven cheese opened a portal to accounting"
+];
 
 function generateResult() {
-    brewCount++;
     const ingredientBox = document.getElementById("ingredients");
-    const ingredients =
-    ingredientBox.value.toLowerCase();
-    const forbiddenCombo =
-    ingredients.includes("milk") &&
-    ingredients.includes("ketchup") &&
-    ingredients.includes("banana");
-    if(forbiddenCombo){
-
-    document.body.classList.add("forbidden-mode");
-
-    const forbiddenMessage =
-        forbiddenResults[
-            Math.floor(Math.random() * forbiddenResults.length)
-        ];
-
-    const textBox =
-        document.querySelector(".scroll-text");
-
-    textBox.textContent = forbiddenMessage;
-
-    resultBox.classList.remove("open");
-    void resultBox.offsetWidth;
-    resultBox.classList.add("open");
-    const loreChance = Math.random();
-    if(loreChance<0.50){
-        setTimeout(showLore,1000);
-    }
-
-
+    const resultBox=document.getElementById("result");
+    
+    if (!ingredientBox.value.trim()) {
+        const TextBox=document.querySelector(".scroll-text");
+        if(textBox){
+        resultBox.textContent = "enter some ingredients first!";
+        }
     return;
 }
-    const resultBox = document.getElementById("result");
-
-    if (!ingredientBox.value.trim()) {
-        resultBox.textContent = "Enter some ingredients first!";
-        return;
+    brewCount++;
+    if(brewCount===1){
+        unlockAchievement("First Brew");
     }
-    
+    if(brewCount===5){
+        unlockAchievement("Curious Apprentice");
+    }
+    if(brewCount===10){
+        unlockAchievement("Potion Enthusiast")
+    }
+    document.body.classList.remove("forbidden-mode");
+
+    const ingredients =
+    ingredientBox.value.toLowerCase();
     const riskFill = document.getElementById("riskFill");
     const riskText = document.getElementById("riskText");
 
     const ingredientCount =
-    ingredientBox.value.split(",").length;
+    ingredientBox.value.split("/[,\s]+/").length;
     const risk=
     Math.min(ingredientCount*20,100);
     if(risk<25){
@@ -106,45 +136,65 @@ function generateResult() {
         riskFill.style.width="100%";
         riskFill.style.background="#ff3333";
         riskText.textContent="🔴 ELDRITCH THREAT";
+        unlockAchievement("Eldritch Chef");
     }
+
+    const forbiddenCombo =
+    ingredients.includes("milk") &&
+    ingredients.includes("ketchup") &&
+    ingredients.includes("banana");
+    const textBox=document.querySelector(".scroll-text");
+    if(forbiddenCombo){
+    document.body.classList.add("forbidden-mode");
+    unlockAchievement("Reality Breaker");
+
+    const forbiddenMessage =
+        forbiddenResults[
+            Math.floor(Math.random() * forbiddenResults.length)
+        ];
+    textBox.textContent = forbiddenMessage;
+
+    resultBox.classList.remove("open");
+    void resultBox.offsetWidth;
+    resultBox.classList.add("open");
+
+    if(Math.random()<0.50){
+        setTimeout(showLore,1000);
+    }
+
+
+    return;
+}
+    
+    
     const randomResult =
         results[Math.floor(Math.random() * results.length)];
 
-    const textBox = document.querySelector(".scroll-text");
     textBox.textContent=randomResult;
     resultBox.classList.remove("open");
     void resultBox.offsetWidth;
     resultBox.classList.add("open");
+    if(Math.random()<0.15){
+        setTimeout(showLore,1000);
+    }
 }
 function enterGrimoire(){
     document.getElementById("warningScreen").style.display="none";
     document.getElementById("mainApp").style.display="block";
 }
-const lorePages =[
-    "PAGE:17 never mix moon ilk with dragon ketchup,the kingdom is still recovering",
-    "PAGE 42:wizard Gary attempted to season soup with pure magic.we do not speak of Gary",
-    "PAGE 88: the great banana incident lasted three winters",
-    "PAGE 103:if your ccauldron begins singing, evacuate immediately",
-    "PAGE 144: the council banned glowing mayonnaise after the incident",
-    "PAGE 207:a potion containing seven cheese opened a portal to accounting"
-]
+
 function showLore(){
+    unlockAchievement("Arcane Researcher");
     const randomLore=
     lorePages[
         Math.floor(Math.random()*lorePages.length)
     ];
-    document.getElementById("loreText").textCount=
+    document.getElementById("loreText").textContent=
     randomLore;
     document.getElementById("lorePopup").style.display=
-    "flex;"
+    "flex";
 }
 function closeLore(){
     document.getElementById("lorePopup").style.display=
     "none";
-}
-
-function closeLore(){
-
-    document.getElementById("lorePopup").style.display =
-        "none";
 }
